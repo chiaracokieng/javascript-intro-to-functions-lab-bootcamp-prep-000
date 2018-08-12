7a6f6e23f5a2150624c0eadd89cca3302b95ec4a
Chiara Cokieng <chiara@chiaracokieng.com>
2018-08-12 00:30:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string === lowercase
+  var string = `hello'
+  string.toLowerCase === string
   return `I can\`t hear you!`
 }
-
-var string = 'hi there'
-string.toLowerCase() === string
